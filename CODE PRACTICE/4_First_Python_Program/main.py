cb126fcfbe5804bcb859a122cd387d876c453cc4
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-23 08:48:39 +0530

    video 4 is completed

--- CODE PRACTICE/4_First_Python_Program/main.py
+++ CODE PRACTICE/4_First_Python_Program/main.py
@@ -1,2 +1,11 @@
-print("Welcome to Python ",name)
 name = input("Enter Your Name: ")
+print(f'Welcome to Python "{name}"')
+
+
+number1 = int(input("Enter First Number: "))
+number2 = int(input("Enter Second Number: "))
+print(f"\nSum of {number1} and {number2} is {number1 + number2}")
+
+
+print("10*20 = ", 10 * 20)
+print(f"number1 and number2 multiplication is {number1 * number2}")
