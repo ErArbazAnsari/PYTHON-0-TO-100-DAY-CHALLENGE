66d7da14679645e0159ba5aa921b9a13bc7c2621
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-24 22:15:16 +0530

    String Methods in python

---
+++ CODE PRACTICE/12_Operations_on_string/operation_on_strings.py
@@ -0,0 +1,20 @@
+# Operations of Strings in python
+
+myString = "Hello World"
+print(myString)
+print(myString[0:5])
+print(myString[:])
+
+# We can find the length of the string using len() function
+# example of len() function
+print(len(myString))
+print(myString[0:11])
+# print(myString[11]) //This will give an error because the index is out of range
+print(myString[10])
+
+print(myString[0:-6])
+print(myString[-1:0])
+
+# Quick Exercise:
+newString = "harry"
+print(newString[-4:-2])
