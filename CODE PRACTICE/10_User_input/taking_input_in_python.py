7207f651ed0bbb9fe20660b86e4caee98d27a9e2
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-24 19:20:56 +0530

    user input() in python

---
+++ CODE PRACTICE/10_User_input/taking_input_in_python.py
@@ -0,0 +1,10 @@
+# a = input()
+a = input("Bro, Please Enter your name: ")
+print("My name is", a)
+print(type(a))
+
+# inputer function always return string type value
+# if you want to take integer input then you have to typecast it
+b = int(input("Enter a number: "))
+print("The number is", b)
+print(type(b))
