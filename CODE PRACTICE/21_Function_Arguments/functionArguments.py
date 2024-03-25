ece57c4d3541319c83659f7894ac5d7cc9d28dc0
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-25 16:49:39 +0530

    21_Function_Arguments

---
+++ CODE PRACTICE/21_Function_Arguments/functionArguments.py
@@ -0,0 +1,32 @@
+# Type of Arguments
+# 1. Default Arguments
+# 2. Keyword Arguments
+# 3. Variable Arguments
+# 4. Required Arguments
+# def average(a, b, c=1):
+#   print("The average is ", (a + b + c) / 2)
+
+
+def average(*numbers):
+    # print(type(numbers))
+    sum = 0
+    for i in numbers:
+        sum = sum + i
+    # print("Average is: ", sum / len(numbers))
+    # return 7
+    return sum / len(numbers)
+
+
+# average(4, 6)
+# average(b=9)
+
+c = average(5, 6, 7, 1)
+print(c)
+
+
+# def name(**name):
+#   # print(type(name))
+#   print("Hello,", name["fname"], name["mname"], name["lname"])
+
+
+# name(mname="Buchanan", lname="Barnes", fname="James")
