08e35573c2c3cabc22d6b62c8aa45a98455e4a98
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-25 13:16:39 +0530

    mySolution of 15_Exercise_2

---
+++ CODE PRACTICE/15_Exercise_2/mySolution.py
@@ -0,0 +1,17 @@
+import time
+
+timeStamp = time.strftime("%H:%M:%S")
+print(timeStamp)
+timeStamp = int(time.strftime("%H"))
+# print(timeStamp)
+# timeStamp = time.strftime('%M')
+# print(timeStamp)
+# timeStamp = time.strftime('%S')
+# print(timeStamp)
+
+if timeStamp <= 12:
+    print("Good, Morning...")
+if timeStamp >= 12 and (timeStamp <= 16):
+    print("Good, Afternoon...")
+if timeStamp >= 16 and timeStamp < 12:
+    print("Good, Night...")
