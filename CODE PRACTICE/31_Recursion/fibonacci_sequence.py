8edba7be8dff77e3e4daed6f75b3d1e94a9694a0
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-26 00:19:24 +0530

    32_Sets_in_python

---
+++ CODE PRACTICE/31_Recursion/fibonacci_sequence.py
@@ -0,0 +1,20 @@
+# Quick Quiz: Write a program to print the Fibonacci sequence
+# f(0) = 0
+# f(1) = 1
+# f(2) = f(1) + f(0)
+# f(n) = f(n-1) + f(n-2)
+
+# 0 1 1 2 3 5 8....
+
+
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n - 1) + fibo(n - 2)
+
+
+print(fibo(2))
+print(fibo(10))
