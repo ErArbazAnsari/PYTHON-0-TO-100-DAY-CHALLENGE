c9243990b92e80dadaabedcf24fb31ce0e1ec0f2
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-25 23:29:12 +0530

    docstrings in python

---
+++ CODE PRACTICE/30_docstrings_in_python/docstrings.py
@@ -0,0 +1,19 @@
+def my_function():
+    """
+    This is a docstring for my_function.
+
+    It provides a brief description of what the function does and any relevant information about its parameters, return values, and behavior.
+    
+    "docstring write just after the function definition line, like in this example"
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    # Function code goes here
+    pass
+
+
+print(my_function.__doc__)
+# Docstring in python
