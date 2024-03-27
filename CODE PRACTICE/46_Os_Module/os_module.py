d9df514529d94a178c80b226fec4f8297b729fec
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-27 16:04:16 +0530

    os os_module

---
+++ CODE PRACTICE/46_Os_Module/os_module.py
@@ -0,0 +1,22 @@
+import os
+
+# if(not os.path.exists("Arbaz")):
+#     os.mkdir("Arbaz")
+    
+# for i in range(0,1000):
+#     os.mkdir(f"Arbaz/{i+1}_Folder")
+
+# for i in range(0,1000):
+#     os.rename(f"Arbaz/{i+1}_Folder", f"Arbaz/{i+1}_FolderName")
+
+
+# folders = os.listdir("Arbaz")
+# print(folders)
+
+# for folder in folders:
+#     print(folder, end=" ")
+#     print(os.listdir(f"Arbaz/{folder}"))
+    
+print(os.getcwd())
+os.chdir("C:")
+print(os.getcwd())
