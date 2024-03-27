a1499ecb4a7024c30df8557f36984329ba04790a
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-27 13:17:26 +0530

    45_if__name=="__main__" in python

---
+++ CODE PRACTICE/45_if__name ==__main__in_python/file.py
@@ -0,0 +1,4 @@
+import welcome as w
+
+# w.hello() welcome.py main hello() fun. phir bhi call hoga.
+w.hello()
---
+++ CODE PRACTICE/45_if__name ==__main__in_python/welcome.py
@@ -0,0 +1,7 @@
+def hello():
+    print("Hi!, you are welcome from python.")
+
+
+print(__name__)
+if __name__ == "__main__":
+    hello()
