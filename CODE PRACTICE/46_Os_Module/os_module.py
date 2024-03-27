f69e7e6afcb014f1196d8566c16e7825a7557a7c
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-27 19:54:30 +0530

    updating files and folder

--- CODE PRACTICE/46_Os_Module/os_module.py
+++ CODE PRACTICE/46_Os_Module/os_module.py
@@ -2,7 +2,7 @@ import os
 
 # if(not os.path.exists("Arbaz")):
 #     os.mkdir("Arbaz")
-    
+
 # for i in range(0,1000):
 #     os.mkdir(f"Arbaz/{i+1}_Folder")
 
@@ -16,7 +16,9 @@ import os
 # for folder in folders:
 #     print(folder, end=" ")
 #     print(os.listdir(f"Arbaz/{folder}"))
-    
+
 print(os.getcwd())
 os.chdir("C:")
 print(os.getcwd())
+
+## Do Refer : https://docs.python.org/3/library/os.html#module-os
