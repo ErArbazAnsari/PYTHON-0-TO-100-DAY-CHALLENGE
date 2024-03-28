d3750a62f306643347bc1ae3d61c497befa7002f
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-28 08:44:45 +0530

    lamda_function

--- CODE PRACTICE/52_Lamda_Function/lamda_function.py
+++ CODE PRACTICE/52_Lamda_Function/lamda_function.py
@@ -2,5 +2,7 @@
 #     return x * 2
 doubleValu = lambda x: x * 2
 newFun = lambda: print("You are my besty")
+average = lambda x, y, z: (x + y + z) / 3
 print(doubleValu(5))
 print(newFun())
+print(average(10, 20, 30))
