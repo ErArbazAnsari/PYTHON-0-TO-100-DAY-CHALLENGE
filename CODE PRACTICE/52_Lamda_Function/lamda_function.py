03c2f2792043498d4c36535d8bae586425d6b09c
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-28 08:51:08 +0530

    lamda_function

--- CODE PRACTICE/52_Lamda_Function/lamda_function.py
+++ CODE PRACTICE/52_Lamda_Function/lamda_function.py
@@ -6,3 +6,11 @@ average = lambda x, y, z: (x + y + z) / 3
 print(doubleValu(5))
 print(newFun())
 print(average(10, 20, 30))
+
+
+def newFunction(fx, value):
+    return 6 + fx(value)
+
+
+x = 2
+print(newFunction((lambda x: x * x), 2))
