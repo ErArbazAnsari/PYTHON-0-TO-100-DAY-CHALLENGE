fd09eae1221c42c50780040b15602e8605f55678
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-28 23:13:31 +0530

    decorators in python

--- CODE PRACTICE/59_Decorators_in_python/decorators_in_python.py
+++ CODE PRACTICE/59_Decorators_in_python/decorators_in_python.py
@@ -1,1 +1,26 @@
 print("Decorators in python.")
+
+
+def greet(fx):
+    def mfx(*args, **kwargs):
+        print("Good Morning")
+        fx(*args, **kwargs)
+        print("Thanks for using this function")
+
+    return mfx
+
+
+@greet
+def hello():
+    print("Hello world")
+
+
+@greet
+def add(a, b):
+    print(a + b)
+
+
+# greet(hello)()
+hello()
+# greet(add)(1, 2)
+add(1, 2)
