1cdcff1c4fd2813ee082d2973ab9683129db055d
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-28 09:23:07 +0530

    is verses equal to in python

---
+++ CODE PRACTICE/54_is_vs_==_in_python/file.py
@@ -0,0 +1,21 @@
+# is and == both are comparision operators
+
+# a = 4
+# b = "4"
+
+# a = [1,2,3]
+# b = [1,2,3]
+
+# a = 4
+# b = 4
+
+# a = (1,2,3)
+# b = (1,2,3)
+
+a = None
+b = None
+print(a is None)
+
+
+print(a is b)  # exact location of object in memory
+print(a == b)  # value
